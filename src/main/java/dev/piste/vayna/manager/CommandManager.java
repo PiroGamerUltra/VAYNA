26e035f9bed3f5a3bc1aaedcc97287ae3cf7ddff
Piste <nils.rypalla@gmail.com>
2023-01-19 20:14:41 +0100

    Edited the CommandManager

--- src/main/java/dev/piste/vayna/manager/CommandManager.java
+++ src/main/java/dev/piste/vayna/manager/CommandManager.java
@@ -4,18 +4,42 @@ import dev.piste.vayna.Bot;
 import net.dv8tion.jda.api.interactions.commands.Command;
 import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class CommandManager {
 
     public static void createCommands() {
 
-        Bot.getJDA().upsertCommand("help", "Get information about the bot and a list of all available commands").queue();
-        SubcommandData connectSubcommandData = new SubcommandData("connect", "Connect your Riot-Games account with your Discord account");
-        SubcommandData infoSubcommandData = new SubcommandData("info", "Get information about the Riot-Games account your Discord account is currently connected with");
-        SubcommandData disconnectSubcommandData = new SubcommandData("disconnect", "Disconnect your currently connected Riot-Games account from your Discord account");
-        Bot.getJDA().upsertCommand("connection", "Connection").addSubcommands(connectSubcommandData, infoSubcommandData, disconnectSubcommandData).queue();
+        List<String> commandList = new ArrayList<>();
+        commandList.add("help");
+        commandList.add("connection");
 
+        for(Command command : Bot.getJDA().retrieveCommands().complete()) {
+            for(String commandName : commandList) {
+                if(!command.getName().equalsIgnoreCase(commandName)) Bot.getJDA().deleteCommandById(command.getIdLong());
+            }
+        }
+
+        for(String commandName : commandList) {
+            createCommand(commandName);
+        }
+
+    }
+
+    private static void createCommand(String commandName) {
+        switch (commandName) {
+            case "help":
+                Bot.getJDA().upsertCommand("help", "Get information about the bot and a list of all available commands").queue();
+                break;
+            case "connection":
+                SubcommandData connectSub = new SubcommandData("connect", "Connect your Riot-Games account with your Discord account");
+                SubcommandData infoSub = new SubcommandData("info", "Get information about the Riot-Games account your Discord account is currently connected with");
+                SubcommandData disconnectSub = new SubcommandData("disconnect", "Disconnect your currently connected Riot-Games account from your Discord account");
+                Bot.getJDA().upsertCommand("connection", "Connection").addSubcommands(connectSub, disconnectSub, infoSub).queue();
+                break;
+        }
     }
 
     public static Command findCommand(String query) {
