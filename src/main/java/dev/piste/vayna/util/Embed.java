c28522913f6473b8c3491d19a9f8aea8fc5b9450
Piste <nils.rypalla@gmail.com>
2023-01-25 21:09:29 +0100

    Updated the custom EmbedBuilder

--- src/main/java/dev/piste/vayna/util/Embed.java
+++ src/main/java/dev/piste/vayna/util/Embed.java
@@ -13,117 +13,80 @@ import java.util.ArrayList;
 
 public class Embed {
 
-    private String description;
-    private String thumbnailUrl;
-    private ArrayList<MessageEmbed.Field> fieldList = new ArrayList<>();
-    private int colorRGB = new Color(157, 57, 191).getRGB();
-    private String title;
-    private String titleUrl;
-    private String authorName;
-    private String authorUrl;
-    private String authorIconUrl;
-    private String imageUrl;
+    private EmbedBuilder embedBuilder;
+    private boolean colorExists = false;
+    private boolean footerExists = false;
 
-    public Embed() {}
+    public Embed() {
+        this.embedBuilder = new EmbedBuilder();
+    }
 
     public Embed setDescription(String description) {
-        this.description = description;
+        embedBuilder.setDescription(description);
         return this;
     }
 
     public Embed setThumbnail(String url) {
-        this.thumbnailUrl = url;
+        embedBuilder.setThumbnail(url);
         return this;
     }
 
     public Embed addField(String name, String text, boolean inline) {
-        MessageEmbed.Field field = new MessageEmbed.Field(name, text, inline);
-        fieldList.add(field);
+        embedBuilder.addField(name, text, inline);
         return this;
     }
 
     public Embed addBlankField(boolean inline) {
-        MessageEmbed.Field field = new MessageEmbed.Field("\u200e", "\u200e", inline);
-        fieldList.add(field);
+        embedBuilder.addField("\u200e", "\u200e", inline);
         return this;
     }
 
     public Embed setColor(int red, int green, int blue) {
-        this.colorRGB = new Color(red, green, blue).getRGB();
+        embedBuilder.setColor(new Color(red, green, blue).getRGB());
+        colorExists = true;
         return this;
     }
 
     public Embed setTitle(String title) {
-        this.title = title;
+        embedBuilder.setTitle(title);
         return this;
     }
 
     public Embed setTitle(String title, String titleUrl) {
-        this.title = title;
-        this.titleUrl = titleUrl;
+        embedBuilder.setTitle(title, titleUrl);
         return this;
     }
 
-
-
     public Embed setAuthor(String name, String url, String iconUrl) {
-        this.authorName = name;
-        this.authorUrl = url;
-        this.authorIconUrl = iconUrl;
+        embedBuilder.setAuthor(name, url, iconUrl);
         return this;
     }
 
     public Embed setAuthor(String name, String url) {
-        this.authorName = name;
-        this.authorUrl = url;
+        embedBuilder.setAuthor(name, url);
         return this;
     }
 
     public Embed setAuthor(String name) {
-        this.authorName = name;
+        embedBuilder.setAuthor(name);
         return this;
     }
 
     public Embed setImage(String url) {
-        this.imageUrl = url;
+        embedBuilder.setImage(url);
+        return this;
+    }
+
+    public Embed setFooter(String text, String iconUrl) {
+        embedBuilder.setFooter(text, iconUrl);
+        footerExists = true;
         return this;
     }
 
     public MessageEmbed build() {
-        EmbedBuilder builder = new EmbedBuilder();
-        for
-        (MessageEmbed.Field field : fieldList) {
-            builder.addField(field);
-        }
-        if (authorName != null) {
-            if (authorUrl != null) {
-                if (authorIconUrl != null) {
-                    builder.setAuthor(authorName, authorUrl, authorIconUrl);
-                } else {
-                    builder.setAuthor(authorName, authorUrl);
-                }
-            } else {
-                builder.setAuthor(authorName);
-            }
-        }
-        if (thumbnailUrl != null) {
-            builder.setThumbnail(thumbnailUrl);
-        }
-        if (title != null) {
-            if (titleUrl != null) {
-                builder.setTitle(title, titleUrl);
-            } else {
-                builder.setTitle(title);
-            }
-        }
-
-        if (imageUrl != null) builder.setImage(imageUrl);
-        builder.setColor(colorRGB);
-        if (description != null) {
-            builder.setDescription(description);
-        }
-        builder.setFooter("VAYNA • VALORANT BOT", Bot.getJDA().getSelfUser().getAvatarUrl());
-        return builder.build();
+        if(!colorExists) embedBuilder.setColor(new Color(157, 57, 191).getRGB());
+        if(!footerExists) embedBuilder.setFooter("VAYNA • VALORANT BOT", Bot.getJDA().getSelfUser().getAvatarUrl());
+        return embedBuilder.build();
 
     }
 
