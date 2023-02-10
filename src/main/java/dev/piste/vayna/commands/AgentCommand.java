b15e2903ee8757946e8aeb92f252291c8bc85af0
Piste <nils.rypalla@gmail.com>
2023-02-11 00:05:38 +0100

    `/agent`: performance fix

--- src/main/java/dev/piste/vayna/commands/AgentCommand.java
+++ src/main/java/dev/piste/vayna/commands/AgentCommand.java
@@ -1,6 +1,5 @@
 package dev.piste.vayna.commands;
 
-import dev.piste.vayna.Bot;
 import dev.piste.vayna.apis.StatusCodeException;
 import dev.piste.vayna.apis.valorantapi.ValorantAPI;
 import dev.piste.vayna.apis.valorantapi.gson.Agent;
@@ -12,6 +11,8 @@ import dev.piste.vayna.util.LanguageManager;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 
 import java.util.ArrayList;
@@ -22,27 +23,30 @@ public class AgentCommand implements Command {
     public void perform(SlashCommandInteractionEvent event) throws StatusCodeException {
         event.deferReply().setEphemeral(true).queue();
         Language language = LanguageManager.getLanguage(event.getGuild());
-        
-        String uuid = ValorantAPI.getAgentByName(event.getOption("name").getAsString(), "en-US").getUuid();
-        Agent agent = ValorantAPI.getAgent(uuid, language.getLanguageCode());
 
+        // Searching the agent (first on english, then in the guild language)
+        Agent agent = ValorantAPI.getAgent(event.getOption("name").getAsString(), language.getLanguageCode());
 
-
+        // Creating the list of embeds to be sent
         ArrayList<MessageEmbed> embedList = new ArrayList<>();
 
+        // Adding the agent embed (general information)
         Embed agentEmbed = new Embed().setAuthor(agent.getDisplayName(), agent.getDisplayIcon())
                 .setDescription(">>> " + agent.getDescription())
                 .setThumbnail(agent.getFullPortrait())
                 .removeFooter();
         embedList.add(agentEmbed.build());
 
+        // Adding the role embed
         Embed roleEmbed = new Embed().setAuthor(agent.getRole().getDisplayName(), agent.getRole().getDisplayIcon())
                 .setDescription(">>> " + agent.getRole().getDescription())
                 .removeFooter();
         embedList.add(roleEmbed.build());
 
         for(Ability ability : agent.getAbilities()) {
+            // Skip if the ability name is "Passive"
             if(ability.getSlot().equalsIgnoreCase("Passive")) continue;
+            // Determine the hotkeys for the different ability types
             String abilityKey;
             switch (ability.getSlot()) {
                 case "Ability1" -> abilityKey = "Q";
@@ -51,12 +55,14 @@ public class AgentCommand implements Command {
                 case "Ultimate" -> abilityKey = "X";
                 default -> abilityKey = "Error";
             }
+            // Adding the ability embed
             Embed abilityEmbed = new Embed().setAuthor(ability.getDisplayName() + " (" + abilityKey + ")", ability.getDisplayIcon())
                     .setDescription(">>> " + ability.getDescription())
                     .removeFooter();
             embedList.add(abilityEmbed.build());
         }
 
+        // Reply with the embed list
         event.getHook().editOriginalEmbeds(embedList).queue();
     }
 
@@ -67,16 +73,15 @@ public class AgentCommand implements Command {
 
     @Override
     public String getDescription() {
-        return "Retrieve general information about a VALORANT agent";
+        return "Get general information about an agent";
     }
 
     @Override
-    public void register() throws StatusCodeException {
-        OptionData optionData = new OptionData(OptionType.STRING, "name", "Name of the agent", true);
+    public CommandData getCommandData() throws StatusCodeException {
+        OptionData optionData = new OptionData(OptionType.STRING, "name", "Agent name", true);
         for(Agent agent : ValorantAPI.getAgents("en-US")) {
-            if(!agent.isPlayableCharacter()) continue;
-            optionData.addChoice(agent.getDisplayName(), agent.getDisplayName());
+            optionData.addChoice(agent.getDisplayName(), agent.getUuid());
         }
-        Bot.getJDA().upsertCommand(getName(), getDescription()).addOptions(optionData).queue();
+        return Commands.slash(getName(), getDescription()).addOptions(optionData);
     }
 }
